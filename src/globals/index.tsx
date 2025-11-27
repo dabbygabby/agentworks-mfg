056f9b0927d36bb0211360d611a16e923943b5a2
Saurabh Dabral <saurabhdabral61@gmail.com>
2025-11-27 07:58:13 +0530

    packaging done

--- src/globals/index.tsx
+++ src/globals/index.tsx
@@ -5,6 +5,7 @@ export const whatsappLinkSheetMetalQuoting = "https://wa.me/919663097918?text=Hi
 
 export const finalCTAMessage = "Hello, I am interested in your product and would like to request a callback to know more."
 export const sheetMetalQuotingMessage = "Hi, I want to know more about the Sheet Metal Quoting Agent."
+export const packagingPrintingMessage = "Hi, I want to test the Packaging Estimator. I have a box spec."
 
 export const whatsappLinkGenerator = (text: string) => {
     return `https://wa.me/919663097918?text=${encodeURIComponent(text)}`;
